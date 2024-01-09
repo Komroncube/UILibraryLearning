9edde3801bd048b303395fb0e0176850738796a4
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-09 21:16:57 +0500

    Add routes for playground component and provide animations in app config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PlaygroundComponent } from './components/playground/playground.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'playground', component: PlaygroundComponent},
+];
