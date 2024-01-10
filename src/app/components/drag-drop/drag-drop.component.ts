c326fafe44b1662c124ae65428b3f1a6761900cb
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-10 11:53:54 +0500

    Add drag and drop functionality to component

---
+++ src/app/components/drag-drop/drag-drop.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { CdkDrag, CdkDropList, CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-drag-drop',
+  templateUrl: './drag-drop.component.html',
+  styleUrls: ['./drag-drop.component.scss'],
+  standalone: true,
+  imports: [CdkDrag, CdkDropList]
+})
+export class DragDropComponent {
+  todo = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ];
+
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+  inprogress:string[] = [];
+
+  drop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex);
+    }
+  }
+}
