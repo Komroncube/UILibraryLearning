bc0a62d9d49603d937ca7e7b3de41a584674b9e9
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-10 11:54:02 +0500

    Add address form and drag-drop components

--- src/app/components/playground/playground.component.ts
+++ src/app/components/playground/playground.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ButtonsComponent } from '../buttons/buttons.component';
 import { DashboardComponent } from "../dashboard/dashboard.component";
+import { AddressFormComponent } from "../address-form/address-form.component";
+import { DragDropComponent } from "../drag-drop/drag-drop.component";
 @Component({
     selector: 'app-playground',
     standalone: true,
     templateUrl: './playground.component.html',
     styleUrl: './playground.component.scss',
-    imports: [ButtonsComponent, DashboardComponent]
+    imports: [ButtonsComponent, DashboardComponent, AddressFormComponent, DragDropComponent]
 })
 export class PlaygroundComponent {
 
